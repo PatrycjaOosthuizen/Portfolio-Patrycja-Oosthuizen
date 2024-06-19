328f14793fd75e7860c84476e55c140c8fe923a9
Patrycja Oosthuizen <pielszym@googlemail.com>
2024-06-19 06:06:50 +0100

    Added carousel with multiple items

---
+++ js/index.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let multipleCardCarousel = document.querySelector(
+    "#carouselCertificatesControls"
+  );
+
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    let carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false, // Disable automatic sliding
+      wrap: false, // Prevent wrapping at the end
+    });
+
+    let carouselWidth = document.querySelector(".carousel-inner").scrollWidth;
+    let cardWidth = document.querySelector(".carousel-item").offsetWidth;
+    let scrollPosition = 0;
+
+    document
+      .querySelector("#carouselCertificatesControls .carousel-control-next")
+      .addEventListener("click", function () {
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+          scrollPosition += cardWidth;
+          document
+            .querySelector("#carouselCertificatesControls .carousel-inner")
+            .scroll({ left: scrollPosition, behavior: "smooth" });
+        }
+      });
+
+    document
+      .querySelector("#carouselCertificatesControls .carousel-control-prev")
+      .addEventListener("click", function () {
+        if (scrollPosition > 0) {
+          scrollPosition -= cardWidth;
+          document
+            .querySelector("#carouselCertificatesControls .carousel-inner")
+            .scroll({ left: scrollPosition, behavior: "smooth" });
+        }
+      });
+  } else {
+    multipleCardCarousel.classList.add("slide");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  let multipleCardCarousel = document.querySelector(
+    "#carouselProjectsControls"
+  );
+
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    let carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false, // Disable automatic sliding
+      wrap: false, // Prevent wrapping at the end
+    });
+
+    let carouselWidth = document.querySelector(".carousel-inner").scrollWidth;
+    let cardWidth = document.querySelector(".carousel-item").offsetWidth;
+    let scrollPosition = 0;
+
+    document
+      .querySelector("#carouselProjectsControls .carousel-control-next")
+      .addEventListener("click", function () {
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+          scrollPosition += cardWidth;
+          document
+            .querySelector("#carouselProjectsControls .carousel-inner")
+            .scroll({ left: scrollPosition, behavior: "smooth" });
+        }
+      });
+
+    document
+      .querySelector("#carouselProjectsControls .carousel-control-prev")
+      .addEventListener("click", function () {
+        if (scrollPosition > 0) {
+          scrollPosition -= cardWidth;
+          document
+            .querySelector("#carouselProjectsControls .carousel-inner")
+            .scroll({ left: scrollPosition, behavior: "smooth" });
+        }
+      });
+  } else {
+    multipleCardCarousel.classList.add("slide");
+  }
+});
