a5611f122d82c38904c40c50403360adada290d8
Patrycja Oosthuizen <pielszym@googlemail.com>
2024-06-19 15:01:01 +0100

    Added footer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML =
+          "<div style = 'color:#00c9ec;font-size:20px; text-align:center'> Thanks for your submission!</div>";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML =
+              "<div style = 'color:#00c9ec'> Oops! There was a problem submitting your form </div>";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML =
+        "<div style = 'color:#00c9ec'> Oops! There was a problem submitting your form </div>";
+    });
+}
+form.addEventListener("submit", handleSubmit);
+
 document.addEventListener("DOMContentLoaded", function () {
   let multipleCardCarousel = document.querySelector(
     "#carouselCertificatesControls"
